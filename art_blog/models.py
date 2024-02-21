1da04cdcfad5348432d39fa726d365ff63bb13dc
PatiTsi <panagiota.tsitsiouli@dci-student.org>
2024-02-21 15:19:46 +0100

    blog views creating

--- art_blog/models.py
+++ art_blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    user_fname = models.ForeignKey("User", on_delete=models.CASCADE)
+    message = models.CharField(max_length=500)
+    time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user_fname} - {self.message}"
--- art_blog/views.py
+++ art_blog/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+# from .models import Item
+from .forms import CommentForm
+from .models import Comment
 
-def home(request):
-    return HttpResponse("""
-        <h1>Welcome to our art_blog</h1>""")
+
+class MainPageView(TemplateView):
+    template_name = "main_page.html"
+
+
+class DataListView(ListView):
+    model = Item
+    template_name = "data_list.html"
+    context_object_name = "items"
+
+
+class DataDetailsView(DetailView):
+    model = Item
+    template_name = "data_details.html"
+    context_object_name = "item"
+
+
+class CommentView(TemplateView):
+    template_name = "comment_box.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save()  # Save the comment to the database
+            return redirect("comment_success")  # Redirect to a success page
+        else:
+            return render(request, self.template_name, {"form": form})
