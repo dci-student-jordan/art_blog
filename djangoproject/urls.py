75644d302b5c82451d63c1aeb3cfec92180f4f7c
dci-student-jordan <dci-student-jordan@dci-student.org>
2024-02-21 15:34:12 +0100

    merge api

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path("art_blog/", include("art_blog.urls")),
 ]
