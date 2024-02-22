ed4c871afb339b571e77a3e77c0c838a87aa3002
Mohsen <mohsen.ajalloueian@dci-student.org>
2024-02-22 11:24:40 +0100

    merg api

--- art_shop/views.py
+++ art_shop/views.py
@@ -1,8 +1,36 @@
 # Create your views here.
-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import products
 
 
 def home(request):
     return HttpResponse(""" <h1>Welcome to our art_blog</h1> """)
+
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h2>This is our Artshop</h2>")
+
+
+def productsView(request):
+    pass
+
+
+
+def product_detail(request,id):
+    pass
+
+
+
+def add_product(request):
+    pass
+
+
+def update_product(request, id):
+    pass
+
+def delete_product(request, id):
+    pass
