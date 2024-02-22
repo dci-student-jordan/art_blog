ad398549d167f6f4c9c5d5229a660f0f065f659c
PatiTsi <panagiota.tsitsiouli@dci-student.org>
2024-02-22 15:44:46 +0100

    api consume

--- art_blog/views.py
+++ art_blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, ListView, DetailView
-
-from art_api.models import ArtWork as Item
 from .forms import CommentForm
 from .models import Comment
+from art_api.models import ArtWork as Item
+from django.conf import settings
 
 
 class MainPageView(TemplateView):
@@ -20,6 +20,13 @@ class DataDetailsView(DetailView):
     model = Item
     template_name = "data_details.html"
     context_object_name = "item"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        item = self.get_object()
+        # context["comments"] = Comment.objects.filter(item=item)
+        context["MEDIA_URL"] = settings.MEDIA_URL
+        return context
 
 
 class CommentView(TemplateView):
